bc6dd99644488425aadeca0bf30b4fdff80a35bd
HaaTa <triplehaata@gmail.com>
2008-10-13 01:08:27 -0700

    Packet Top-Level Encoder and Decoder written

--- src/Baulk/Server/packet.cpp
+++ src/Baulk/Server/packet.cpp
@@ -22,8 +22,101 @@
 
 // Constructors
 Packet::Packet( QString packet, QObject *parent ) : QObject( parent ) {
+	// TODO - Sanity Check on Packet
+	packetS = packet;
+
+	redecode = true;
+	reencode = false;
 }
 
 Packet::Packet( QString destinationId, QString senderId, QStringList dataFlags, QStringList data, QObject *parent ) : QObject( parent ) {
+	// TODO - Sanity Check on Packet Pieces
+	destinationIdS = destinationId;
+	senderIdS = senderId;
+	dataFlagsS = dataFlags;
+	dataS = data;
+
+	redecode = false;
+	reencode = true;
+}
+
+// Public
+
+// Private
+bool Packet::dataFlagsSanityStore( QStringList dataFlags ) {
+}
+
+bool Packet::dataSanityStore( QStringList data ) {
+}
+
+void Packet::debugOutput() {
+}
+
+void Packet::decode() {
+	if ( !redecode )
+		return;
+	
+	QStringList tmp = packetS.split("|");
+	QList<bool> success;
+
+	// Decypher TO and FROM Ids
+	success << destinationIdSanityStore( tmp[0].remove("TO:") );
+	success << senderIdSanityStore( tmp[1].remove("FROM:") );
+
+	// If Data and Data Flags are contained in the packet
+	if ( tmp.count() > 2 ) {
+		QStringList data;
+		QStringList flags;
+		QStringList tmp2;
+
+		for ( int c = 2; c < tmp.count(); ++c ) {
+			tmp2 = tmp[c].split(":");
+			flags << tmp2[0];
+			data << tmp2[1];
+		}
+
+		success << dataFlagsSanityStore( flags );
+		success << dataSanityStore( data );
+	}
+
+	// If a single sanity fails, output debug info
+	for ( int c = 0; c < success.count(); ++c )
+		if ( !success[c] )
+			debugOutput();
+
+	redecode = false;
+}
+
+bool Packet::destinationIdSanityStore( QString destinationId ) {
+}
+
+void Packet::encode() {
+	if ( !reencode )
+		return;
+
+	QString ids = QString("TO:%1|FROM:%2").arg( destinationIdS ).arg( senderIdS );
+
+	if ( dataFlagsS.count() != dataS.count() ) {
+		qWarning( tr("InformationPacket\n\t|There are not equal number(s) of Data Flags and Data").toUtf8() );
+		ids = "INVALID";
+	}
+
+	// Add data and data flags to the packet, if they exist 
+	if ( dataS.count() > 0 ) {
+
+		// Generate data + dataflags portion of the packet
+		QString dataC = "";
+		for ( int c = 0; c < dataS.count(); ++c )
+			dataC += QString("|%1:%2").arg( dataFlagsS[c] ).arg( dataS[c] );
+
+		ids += dataC;
+	}
+
+	packetS = ids;
+
+	reencode = false;
+}
+
+bool Packet::senderIdSanityStore( QString senderId ) {
 }
 
--- src/Baulk/Server/packet.h
+++ src/Baulk/Server/packet.h
@@ -9,6 +9,29 @@
 //	Encodes/Decodes BaulkPackets for use with the Baulk Information
 //	Server.
 //
+//
+//  Packet Details:
+//
+//  TO:<destination ID>|FROM:<source ID>|<data flag>:<data>
+//
+//  or
+//
+//  TO:<destination ID>|FROM:<source ID>|<data flag>:<data>|<data flag2>
+//  	:<data>|<data flag3>:<data>| etc...
+//
+//
+//  TO/FROM Id Details:
+//
+//  Screen{<screen number>}Inc{<increment>}
+//  	ie. Screen{0}Inc{2} for the second window on Screen Zero
+//
+//	If the first window is deleted ( Screen{0}Inc{1} ), then the next window 
+//		created will be Screen{0}Inc{1}
+//
+//	The Increment is not defined by the screen. It is the global increment (so 
+//		for the above ie. there will be no Screen{1}Inc{2})
+//
+//  
 //  Baulk is free software; you can redistribute it and/or modify
 //  it under the terms of the GNU General Public License as published by
 //  the Free Software Foundation; either version 2 of the License, or
@@ -25,6 +48,7 @@
 #ifndef __PACKET_H
 #define __PACKET_H
 
+#include <QList>
 #include <QObject>
 #include <QStringList>
 
@@ -36,9 +60,28 @@ public:
 	Packet( QString packet, QObject *parent = 0 );
 	// Encode Packet Constructor
 	// 	- Note: Each String of data MUST be accompanied with a dataFlag string
-	Packet( QString destinationId, QString senderId, QStringList dataFlags, QStringList data, QObject *parent = 0 );
+	Packet( QString destinationId, QString senderId, QStringList dataFlags = QStringList(), QStringList data = QStringList(), QObject *parent = 0 );
 
 private:
+	bool redecode;
+	bool reencode;
+
+	QString packetS;
+	QString destinationIdS;
+	QString senderIdS;
+
+	QStringList dataFlagsS;
+	QStringList dataS;
+
+	bool dataFlagsSanityStore( QStringList dataFlags );
+	bool dataSanityStore( QStringList data );
+	bool destinationIdSanityStore( QString destinationId );
+	bool senderIdSanityStore( QString senderId );
+
+	void debugOutput();
+
+	void decode();
+	void encode();
 
 };
 
